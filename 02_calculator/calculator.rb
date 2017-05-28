39ec295b599fc6ef1052cb3993635acceebde3b4
Doug <doug.steinberg@gmail.com>
2017-05-28 16:07:52 -0400

    02 calculator complete

---
+++ 02_calculator/calculator.rb
@@ -0,0 +1,21 @@
+def subtract(num1, num2)
+
+  diff = num1 - num2
+
+end
+
+
+def add(num1, num2)
+
+  sum = num1 + num2
+
+end
+
+
+def sum(num_array)
+  tot = 0
+  num_array.each do |num|
+    tot += num
+  end
+  tot
+end
